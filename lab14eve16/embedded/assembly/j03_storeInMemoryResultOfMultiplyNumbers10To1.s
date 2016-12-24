a91154e0c4bfd6543db5d6780189791844084f06
byod <byod>
2016-12-24 13:01:23 +0530

    corrected the header section to ensure the assembler allocates memory in data block

--- lab14eve16/embedded/assembly/j03_storeInMemoryResultOfMultiplyNumbers10To1.s
+++ lab14eve16/embedded/assembly/j03_storeInMemoryResultOfMultiplyNumbers10To1.s
@@ -1,21 +1,24 @@
 ; This program multiplies the numbers 10+9+8.....+1
-; At the same time stores the results in successive memory locations
+; At the same time stores the results in a give memory location, overwrites location with every loop
 ; Three registers are used R1, R2 and R3
 ; R2 is used as the multiplicand register
 ; R3 is used as the multiplier register
 ; The final result is stored in R1
+		AREA |Header Data|, DATA
+		ALIGN 4
+		ENTRY
+MYDATA  EQU 0x10001000 ; We should know the typical data memory block address to manually set the value
+MYDATA1 DCD 0 ; DCD will correctly allocate the memory in data block, the above AREA should declare that it is DATA
+		
 		AREA |.text|, CODE
 		EXPORT storeInMemoryResultOfMultiplyNumbers10To1
-		ENTRY
-MYDATA EQU 0x10001000 ; DCD instruction was allocating memory within the code block, hence manually setting location
-MYDATA1 DCD 0
 		
 storeInMemoryResultOfMultiplyNumbers10To1
 		MOV R1, #0   ; Initialize the result value with 0
 		MOV R2, #1   ; Initialize the multiplicand with 1
 		MOV R3, #10  ; Use R3 as the multiplier, as well as counter or start number
 		LDR R4, =MYDATA ; Use R4 as the address pointer
-		LDR R5, =MYDATA1 ;  Observation is that the DCD allocation location within code block, hence does not work
+		LDR R5, =MYDATA1 ;  DCD allocation is in data block, this also can be used
 		
 multiplyLoop
 		MUL R1, R2, R3 ; Multiply R2 and R3 and store in R1
